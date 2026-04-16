f62402e30386f1f24a12a2c271492f2b5a61f107
Yohannes Teferi <yteferi964@gmail.com>
2026-04-16 10:38:15 +0300

    path parameters in fast api

--- books.py
+++ books.py
@@ -3,12 +3,31 @@ from fastapi import FastAPI
 
 app=FastAPI()
 
-@app.get("/api-endpoint")
-async def first_api():
-    return (
-        
-        {
-            "msg":"hello yoni"
-        }
-    )
+
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+@app.get("/books")
+
+async def read_all_books():
+    return BOOKS
+
+
+@app.get('/books/mybook')
+async def read_all_books():
+    return {'book_title':'my favorite book ! '}
+
+@app.get('/books/{dynamic_param}')
+async def read_all_books(dynamic_param:str):
+    return {'dynamic_param':dynamic_param}
+
+
+
 
