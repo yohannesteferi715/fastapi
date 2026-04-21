4bd1d7e154b7215470f7c102db1aec2df0913fc7
Yohannes Teferi <yteferi964@gmail.com>
2026-04-21 15:04:24 +0300

    fast api pydantics and data validation

--- books.py
+++ books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import   FastAPI,Body
 
 
 app=FastAPI()
@@ -26,7 +26,7 @@ async def read_all_books():
 
 
 
-@app.get('/books/{book_title: str}')
+@app.get('/books/{book_title}')
 
 async def read_book(book_title):
     
@@ -52,3 +52,40 @@ async def read_category_by_query(category: str):
 
 
 
+@app.get("/books/{book_author}")
+
+async def  read_author_category_by_query(book_author:str,category:str):
+    books_to_return=[]
+    
+    for book in BOOKS:
+        if( book.get('author').casefold()==book_author.casefold() and 
+            book.get('category').casefold()==category.casefold()):
+            books_to_return.append(book)
+            
+    return  books_to_return
+            
+            
+           
+        
+
+   
+
+@app.post("/books/create_book")
+async  def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+ 
+ 
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+            
+            
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
---
+++ books2.py
@@ -0,0 +1,66 @@
+from fastapi import FastAPI,Body
+
+from pydantic import BaseModel
+
+
+app=FastAPI()
+
+
+
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+    published_date: int
+
+    def __init__(self, id, title, author, description, rating, published_date):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+        self.published_date = published_date
+
+class BookRequest(BaseModel):
+    id:int
+    title:str
+    description:str
+    rating:int
+    
+
+    
+
+
+BOOKS = [
+    Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5, 2030),
+    Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5, 2030),
+    Book(3, 'Master Endpoints', 'codingwithroby', 'A awesome book!', 5, 2029),
+    Book(4, 'HP1', 'Author 1', 'Book Description', 2, 2028),
+    Book(5, 'HP2', 'Author 2', 'Book Description', 3, 2027),
+    Book(6, 'HP3', 'Author 3', 'Book Description', 1, 2026)
+]
+
+
+
+
+@app.get("/books")
+
+async  def read_all_books():
+    
+    return  BOOKS
+
+
+
+@app.post("/create_boook")
+
+async  def create_book(book_request:BookRequest):
+    new_book=Book(**book_request.model_dump())
+    print(type(new_book))
+    BOOKS.append(new_book)
+    
+
+
+
